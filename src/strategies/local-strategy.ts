9c05dcbb633a0f5770d212ccb8aa04c14daa7d7a
TriBhaskar <panthribhaskar@gmail.com>
2024-08-06 23:25:16 +0530

    feat: Improve local strategy for passport authentication
    The code changes in `local-strategy.ts` enhance the local strategy for passport authentication. The `passport.use()` function now includes a callback that searches for a user in the `mockUsers` array based on the provided username. If a user is not found, an error is thrown. This improvement strengthens the authentication process and ensures that only valid users can authenticate.

--- src/strategies/local-strategy.ts
+++ src/strategies/local-strategy.ts
@@ -1,6 +1,16 @@
-import passport  from "passport";
+import passport from "passport";
 import { Strategy } from "passport-local";
+import { mockUsers } from "../utils/constants";
 
 passport.use(
-    new Strategy(username:str, password, done)=>
-)
+  new Strategy((username, password, done) => {
+    try {
+      const findUser = mockUsers.find((user) => user.username === username);
+      if (!findUser) {
+        throw new Error("User not found");
+      }
+    } catch (error) {
+      return done(error);
+    }
+  })
+);
